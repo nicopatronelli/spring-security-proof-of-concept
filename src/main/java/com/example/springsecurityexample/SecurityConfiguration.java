664cba3f94869620eef7eb4cb4947b7d9533db0e
nicopatronelli <nicopatronelli@gmail.com>
2020-09-29 00:28:22 -0300

    minor changes

--- src/main/java/com/example/springsecurityexample/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurityexample/SecurityConfiguration.java
@@ -11,13 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-//    @Autowired
-//    private DataSource dataSource;
 
     @Qualifier("myUserDetailsService")
     @Autowired
